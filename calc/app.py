040f1ef21d7380df98fbdca15ff85b02720d8255
Logan Lanni <style907@Logans-Air.home>
2023-01-16 16:34:09 -0800

    added files for calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,38 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+
+app.route('/')
+def home():
+    return "Homepage"
+
+@app.route('/add')
+def add_route():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = add(a,b)
+    return  str(result)
+
+@app.route('/sub')
+def sub_route():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = sub(a,b)
+    return  str(result)
+
+@app.route('/mult')
+def mult_route():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = mult(a,b)
+    return  str(result)
+
+@app.route('/div')
+def div_route():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = div(a,b)
+    return  str(result)
